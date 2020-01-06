436c2e2253a00198dd5e5d02740d352af0d5ae33
sindafekir <40177956+sindafekir@users.noreply.github.com>
2020-01-06 12:49:38 -0500

    pixel intensity extraction of non-vascular space

--- BBBpermTS.m
+++ BBBpermTS.m
@@ -108,5 +108,84 @@ while segQ == 1
     segQ = input('Does segmentation need to be redone? Yes = 1. No = 0. ');    
 end 
 
+%% invert the mask
+BWstacksInv = cell(1,length(ROIstacks));
+for Z = 1:length(ROIstacks)
+    for trialType = 1:size(inputStacks{z},2)
+        for VROI = 1:numROIs 
+            if isempty(inputStacks{z}{trialType}) == 0 
+                for trial = 1:size(ROIstacks{Z}{trialType},2)
+                        for frame = 1:size(ROIstacks{Z}{trialType}{trial}{VROI}{1},3)                            
+                            BWstacksInv{Z}{trialType}{trial}{VROI}(:,:,frame) = ~(BWstacks{Z}{trialType}{trial}{VROI}(:,:,frame)); 
+                        end 
+                end 
+            end 
+        end 
+    end 
+end  
+
+%% get pixel intensity value of extravascular space 
+meanPixIntArray = cell(1,length(ROIstacks));
+for Z = 1:length(ROIstacks)
+    for trialType = 1:size(inputStacks{z},2)
+        for VROI = 1:numROIs 
+            if isempty(inputStacks{z}{trialType}) == 0 
+                for trial = 1:size(ROIstacks{Z}{trialType},2)
+                        for frame = 1:size(ROIstacks{Z}{trialType}{trial}{VROI}{1},3)                            
+                            stats = regionprops(BWstacksInv{Z}{trialType}{trial}{VROI}(:,:,frame),ROIstacks{Z}{trialType}{trial}{VROI}{1}(:,:,frame),'MeanIntensity');
+                            ROIpixInts = zeros(1,length(stats));
+                            for stat = 1:length(stats)
+                                ROIpixInts(stat) = stats(stat).MeanIntensity;
+                            end 
+                            meanPixIntArray{Z}{trialType}{trial}{VROI}(frame) = mean(ROIpixInts);    
+                        end 
+                        % turn all rows full of zeros into NaNs 
+                        allZeroRows = find(all(meanPixIntArray{Z}{trialType}{trial}{VROI} == 0,2));
+                        for row = 1:length(allZeroRows)
+                            meanPixIntArray{Z}{trialType}{trial}{VROI} = NaN; 
+                        end 
+                end 
+            end 
+        end 
+    end 
+end  
+
+
+
+
+
+
+           
+%% normalize and z score 
+            
+dataMeds = cell(1,length(ROIstacks));
+DFOF = cell(1,length(ROIstacks));
+dataSlidingBLs = cell(1,length(ROIstacks));
+DFOFsubSBLs = cell(1,length(ROIstacks));
+zData = cell(1,length(ROIstacks));
+for Z = 1:length(ROIstacks)
+    for trialType = 1:size(inputStacks{z},2)
+        for VROI = 1:numROIs 
+            if isempty(inputStacks{z}{trialType}) == 0 
+                for trial = 1:size(ROIstacks{Z}{trialType},2)
+                    %get median value per trace
+                    dataMed = nanmedian(meanPixIntArray{Z}{trialType}{trial}{VROI});     
+                    dataMeds{Z}{trialType}{trial}{VROI} = dataMed;
+                    %compute DF/F using median  
+                    DFOF{Z}{trialType}{trial}{VROI} = (meanPixIntArray{Z}{trialType}{trial}{VROI}-dataMeds{Z}{trialType}{trial}{VROI})./dataMeds{Z}{trialType}{trial}{VROI};   
+                    %get sliding baseline 
+                    [dataSlidingBL]=slidingBaseline(DFOF{Z}{trialType}{trial}{VROI},floor((FPS/numZplanes)*10),0.5); %0.5 quantile thresh = the median value                 
+                    dataSlidingBLs{Z}{trialType}{trial}{VROI} = dataSlidingBL;    
+                    %subtract sliding baseline from DF/F
+                    DFOFsubSBLs{Z}{trialType}{trial}{VROI} = DFOF{Z}{trialType}{trial}{VROI}-dataSlidingBLs{Z}{trialType}{trial}{VROI};                   
+                    %z-score data 
+                    zData{Z}{trialType}{trial}{VROI} = zscore(DFOFsubSBLs{Z}{trialType}{trial}{VROI});
+                end 
+            end 
+        end 
+    end 
+end 
+
+
 
 end 
