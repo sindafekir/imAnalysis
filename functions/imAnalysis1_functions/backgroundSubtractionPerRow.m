e553803715a6cd58c38d6cdf2a70de0f82d52153
sindafekir <40177956+sindafekir@users.noreply.github.com>
2020-07-19 11:09:04 -0400

    the code for selecting multiple ROIs is done and user friendly
    working on iterating through multiple planes in z. this is annoying to have to do semi-manually, but the CA ROIs differ in different planes in z, so it must be done this way--for now...

--- functions/imAnalysis1_functions/backgroundSubtractionPerRow.m
+++ functions/imAnalysis1_functions/backgroundSubtractionPerRow.m
@@ -24,93 +24,85 @@ if blackOutCaROIQ == 1
     end    
 end 
 
-% display prompt so user knows what to do 
-disp('Select rows that do not contain vessels for background subtraction.')
-% select rows that do not contain vessels for background subtraction -
-% create mask 
-[ROI_stack,xmin,ymin,width,height] = firstTimeCreateROIs(1,reg__Stacks{1});
-it = 1; 
-ROIstacks{it} = ROI_stack;
-xmins(it) = xmin;
-ymins(it) = ymin;
-% determine what rows were selected so far 
-ymax = height + ymin - 1;
-xmax = width + xmin - 1;
-xmaxs(it) = xmax;
-ymaxs(it) = ymax;
-rowsSelected = ymin:ymax;
-% if all rows were not selected display what rows were selected and give
-% option to select more rows 
-allRows = 1:size(reg__Stacks{1},1); % row indices possible 
-while length(rowsSelected) < length(allRows) % if there are less rows selected than the total number of rows possible
-    % find the rows that were not selected 
-    rowsNotSelected = ~ismember(allRows,rowsSelected); 
-    RowInds = find(rowsNotSelected);
-    % let the user know what rows still need selection 
-    disp('These rows still need selection:')
-    disp(RowInds)                    
-    %overlay background subtraction ROIs (so far) on image of FOV 
-    subplot(1,2,1)
-    imshow(reg__Stacks{1}(:,:,1),[0 1000]);
-    hold all; 
-    for i = 1:it
-        x = [xmins(i),xmaxs(i),xmaxs(i),xmins(i),xmins(i)];
-        y = [ymins(i),ymins(i),ymaxs(i),ymaxs(i),ymins(i)];
-        plot(x,y,'r','LineWidth',2) 
-    end
-    % select additional rows that do not contain vessels for background subtraction -
-    % create mask
-    subplot(1,2,2)
+% @@@@@@@@@@@@@@@@@@@
+% BELOW NEEDS EDITING 
+% @@@@@@@@@@@@@@@@@@@
+
+% TO DO:
+% FINISH ADDING IN ABILITY TO ITERATE THROUGH Z PLANES 
+% TEST THE CODE 
+
+% select rows for background subtraction - this code goes through multiple
+% rounds of row selection - make sure to select rows where they do not have
+% blacked out pixels (from the calcium ROI removal) or vessels 
+for z = 1:length(CaROImasks)
+    disp(fprintf('This is Z plane #%d.',z))
+    % display prompt so user knows what to do 
+    disp('Select rows that do not contain vessels for background subtraction.')
+    % select rows that do not contain vessels for background subtraction -
+    % create mask 
     [ROI_stack,xmin,ymin,width,height] = firstTimeCreateROIs(1,reg__Stacks{1});
-    it = it + 1;
+    it = 1; 
     ROIstacks{it} = ROI_stack;
     xmins(it) = xmin;
     ymins(it) = ymin;
+    widths(it) = width;
+    heights(it) = height;
     % determine what rows were selected so far 
     ymax = height + ymin - 1;
     xmax = width + xmin - 1;
     xmaxs(it) = xmax;
     ymaxs(it) = ymax;
-    rowsSelected = [rowsSelected,ymin:ymax];
-end 
-
-%@@@@@@@@@@@@@@@@@@@@
-% @@@@@@@@@@@@@@@@@@@
-% BELOW NEEDS EDITING 
-% @@@@@@@@@@@@@@@@@@@
-
-% NEED TO:
-% FIGURE OUT WHAT TO DO ABOUT OVERLAPPING ROW SELECTION 
-
-% apply mask to each frame to get background pixel intensity per row 
-BG_ROIboundData = cell(1,length(reg__Stacks));
-BG_ROIstacks = cell(1,length(reg__Stacks));
-for stack = 1:length(reg__Stacks)
-    data = reg__Stacks{stack};
-    if stack == 1
-        [ROI_stacks,xmins,ymins,widths,heights] = firstTimeCreateROIs(1,data);
-        BG_ROIboundData{stack}{1} = xmins;
-        BG_ROIboundData{stack}{2} = ymins;
-        BG_ROIboundData{stack}{3} = widths;
-        BG_ROIboundData{stack}{4} = heights;
-        BG_ROIstacks{stack} = ROI_stacks;
-
-    elseif stack > 1 
-        BG_ROIboundData{stack}{1} = BG_ROIboundData{1}{1};
-        BG_ROIboundData{stack}{2} = BG_ROIboundData{1}{2};
-        BG_ROIboundData{stack}{3} = BG_ROIboundData{1}{3};
-        BG_ROIboundData{stack}{4} = BG_ROIboundData{1}{4};
-
-        xmins = BG_ROIboundData{stack}{1};
-        ymins = BG_ROIboundData{stack}{2};
-        widths = BG_ROIboundData{stack}{3};
-        heights = BG_ROIboundData{stack}{4};
-        [ROI_stacks] = make_ROIs_notfirst_time(data,xmins,ymins,widths,heights);
-        BG_ROIstacks{stack} = ROI_stacks;
+    rowsSelected = ymin:ymax;
+    % if all rows were not selected display what rows were selected and give
+    % option to select more rows 
+    allRows = 1:size(reg__Stacks{1},1); % row indices possible 
+    while length(rowsSelected) < length(allRows) % if there are less rows selected than the total number of rows possible
+        % find the rows that were not selected 
+        rowsNotSelected = ~ismember(allRows,rowsSelected); 
+        RowInds = find(rowsNotSelected);
+        % let the user know what rows still need selection 
+        disp('These rows still need selection:')
+        disp(RowInds)                    
+        %overlay background subtraction ROIs (so far) on image of FOV 
+        subplot(1,2,1)
+        imshow(reg__Stacks{1}(:,:,1),[0 1000]);
+        hold all; 
+        for i = 1:it
+            x = [xmins(i),xmaxs(i),xmaxs(i),xmins(i),xmins(i)];
+            y = [ymins(i),ymins(i),ymaxs(i),ymaxs(i),ymins(i)];
+            plot(x,y,'r','LineWidth',2) 
+        end
+        % select additional rows that do not contain vessels for background subtraction -
+        % create mask
+        subplot(1,2,2)
+        [ROI_stack,xmin,ymin,width,height] = firstTimeCreateROIs(1,reg__Stacks{1});
+        it = it + 1;
+        ROIstacks{it} = ROI_stack;
+        xmins(it) = xmin;
+        ymins(it) = ymin;
+        % determine what rows were selected so far 
+        ymax = height + ymin - 1;
+        xmax = width + xmin - 1;
+        xmaxs(it) = xmax;
+        ymaxs(it) = ymax;
+        widths(it) = width;
+        heights(it) = height;
+        rowsSelected = [rowsSelected,ymin:ymax];
     end 
+    BG_ROIboundData{z}{1} = xmins;
+    BG_ROIboundData{z}{2} = ymins;
+    BG_ROIboundData{z}{3} = widths;
+    BG_ROIboundData{z}{4} = heights;
 end 
 
 %@@@@@@@@@@@@@@@@@@@@
+
+
+% NEED TO EVENTUALLY:
+% FIGURE OUT WHAT TO DO ABOUT OVERLAPPING ROW SELECTION - FOR NOW I DON'T
+% HAVE ANY OVERLAP SO GET ALL BELOW CODE WORKING WITHOUT OVERLAP FIRST AND THEN MAKE IT
+% MORE COMPLICATED 
 
 
 % determine average pixel intensity of each frame and row in the control
