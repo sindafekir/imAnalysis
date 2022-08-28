c59697aa91f70d479f377778d5f3162a93e3e39f
sindafekir <40177956+sindafekir@users.noreply.github.com>
2022-08-28 15:46:30 -0400

    working changes

--- calciumVwidthBBBanalysis.m
+++ calciumVwidthBBBanalysis.m
@@ -1763,7 +1763,7 @@ end
 % smoothing/normalizing below 
 % will separate data based on trial number and ITI length (so give it all
 % the trials per mous
-
+%{
 %get the data you need 
 mouseNum = input('How many mice are there? ');
 CAQ = input('Input 1 if there is Ca data to plot. ');
